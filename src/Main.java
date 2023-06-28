4dca189e8919ec7e0b698cfec0208124c5fbd7b6
E-jiwoo <livei07@naver.com>
2023-06-29 00:10:45 +0900

    feat(add input and output ){ -학생의 이름과 성적, 출결, 가산점을 입력받고 출력한다 }

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 package src;
+
+import java.util.Scanner;
+
+import static src.Student.*;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        name = sc.next();
+        score = sc.nextInt();
+        check = sc.nextInt();
+        Student.point = sc.nextInt();
 
+        Student student = new Student(); // Student 객체 생성
+        student.studentResult(name, score, check, point); // result 메서드 호출
     }
 }
