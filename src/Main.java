716299a3db41353aa97d36b76c1d5853baf6d902
E-jiwoo <livei07@naver.com>
2023-06-29 00:21:05 +0900

    feat(add input and output){ -지원 대학교의 이름과 지원학과를 입력받고 출력한다 }

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,22 @@ package src;
 import java.util.Scanner;
 
 import static src.Student.*;
+import static src.University.department;
+import static src.University.universityName;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        universityName = sc.next();
+        University.department = sc.next();
         name = sc.next();
         score = sc.nextInt();
         check = sc.nextInt();
         Student.point = sc.nextInt();
 
+        University university = new University(); // university 객체 생성
+        university.universityResult(universityName, department); // universityResult 메서드 호출
         Student student = new Student(); // Student 객체 생성
-        student.studentResult(name, score, check, point); // result 메서드 호출
+        student.studentResult(name, score, check, point); // studentResult 메서드 호출
     }
 }
---
+++ src/University.java
@@ -0,0 +1,11 @@
+package src;
+
+public class University {
+    public static String universityName;
+    public static String department;
+
+    public void universityResult(String universityName, String department){
+        System.out.printf("지원대학교: %s\n지원학과: %s\n", universityName, department);
+    }
+
+}
